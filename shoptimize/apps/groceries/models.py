ee592578f9f1254a7db490fb9e7273e55a74428c
Dariusz Suszczewicz <dariusz.suszczewicz@gmail.com>
2018-01-27 22:58:18 +0100

    added discount model

--- shoptimize/apps/groceries/models.py
+++ shoptimize/apps/groceries/models.py
@@ -7,3 +7,10 @@ class GroceryItem(models.Model):
 
 class GroceryStore(models.Model):
     name = models.CharField(max_length=32, unique=True)
+
+
+class GroceryDiscount(models.Model):
+    item = models.ForeignKey(to=GroceryItem, on_delete=models.CASCADE)
+    store = models.ForeignKey(to=GroceryStore, on_delete=models.CASCADE)
+    price_before = models.DecimalField(max_digits=8, decimal_places=2)
+    price_after = models.DecimalField(max_digits=8, decimal_places=2)
--- shoptimize/apps/groceries/test_models.py
+++ shoptimize/apps/groceries/test_models.py
@@ -13,3 +13,10 @@ def test_create_grocery_item():
 def test_create_grocery_store():
     store = models.GroceryStore.objects.create(name="Lidl")
     assert store.name == "Lidl"
+
+
+@pytest.mark.django_db
+def test_create_discount():
+    store = models.GroceryStore.objects.create(name="Lild")
+    item = models.GroceryItem.objects.create(name="Ser żółty Podlaski")
+    discount = models.GroceryDiscount.objects.create(item=item, store=store, price_before=1.00, price_after=0.95)
