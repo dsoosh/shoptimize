4f7caf2711bb55db01d816201256976b67fbc46e
Dariusz Suszczewicz <dariusz.suszczewicz@gmail.com>
2018-01-28 11:29:40 +0100

    Changed discount percentage and amount to proepr fields

--- shoptimize/apps/groceries/models.py
+++ shoptimize/apps/groceries/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from decimal import Decimal
 
 
 class GroceryItem(models.Model):
@@ -15,11 +14,11 @@ class GroceryDiscount(models.Model):
     store = models.ForeignKey(to=GroceryStore, on_delete=models.CASCADE)
     price_before = models.DecimalField(max_digits=8, decimal_places=2)
     price_after = models.DecimalField(max_digits=8, decimal_places=2)
+    percent = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
 
-    @property
-    def percent(self):
-        return round(1 - self.price_after / self.price_before, 2) * 100
-
-    @property
-    def amount(self):
-        return round(self.price_before - self.price_after, 2)
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.percent = round(1 - self.price_after / self.price_before, 2) * 100
+        self.amount = round(self.price_before - self.price_after, 2)
+        super(GroceryDiscount, self).save()
